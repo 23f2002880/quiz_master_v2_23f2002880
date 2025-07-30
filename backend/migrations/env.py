1c0c6adb6901da3cb3eafe97346a47880a82a36b
23f2002880 <23f2002880@ds.study.iitm.ac.in>
2025-07-31 05:07:59 +0530

    Added migrations

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,11 +5,11 @@ from flask import current_app
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
+# this is the Alembic Config objects, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
+# Interpret the config files for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
